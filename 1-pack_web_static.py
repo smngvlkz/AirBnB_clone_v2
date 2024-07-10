ba0efedea2d762e32e2d207ea2d8d2ad8612d329
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-10 15:21:53 +0200

    Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-# Generates a .tgz archive from the contents of the web_static folder
+"""
+Generates a .tgz archive from the contents of the web_static folder
+"""
 
 from fabric.api import local
 from datetime import datetime
 import os
 
 def do_pack():
-    """Generates a .tgz archive from the contents of the web_static folder."""
-    # Creates the versions folder if it doesn't exist
-    if not os.path.exists("versions"):
-        os.makedirs("versions")
-    
-    # Creates the archive name
-    now = datetime.now()
-    archive_name = "versions/web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
-    
-    # Creates the archive
-    result = local("tar -cvzf {} web_static".format(archive_name))
-    
-    # Checks if the archive was created successfully
-    if result.succeeded:
-        return archive_name
-    else:
+    """
+    Generates a .tgz archive from the contents of the web_static folder
+    """
+    try:
+        # Creates the versions folder if it doesn't exist
+        local("mkdir -p versions")
+
+        # Generates the archive name with timestamp
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_name = "versions/web_static_{}.tgz".format(date)
+
+        # Creates the archive
+        local("tar -cvzf {} web_static".format(archive_name))
+
+        # Checks if the archive was created successfully
+        if os.path.exists(archive_name):
+            return archive_name
+        else:
+            return None
+    except:
         return None
