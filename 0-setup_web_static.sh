45644e2b6d10a0d62848ec0a494b11d4f78bf2c2
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-10 18:42:42 +0200

    Prepare your web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -30,10 +30,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
 
 # Updates Nginx configuration
-sudo sed -i '/server_name _;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/341096-web-01 _;/a \\\n\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # Tests Nginx configuration and restart if successful
 sudo nginx -t && sudo service nginx restart
-
-# Exits successfully
-exit 0
