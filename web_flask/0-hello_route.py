0c4738040df0f2afbef0dd3d9a9f16e5fb73f82a
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-25 12:01:14 +0200

    Hello Flask!

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Start the Flask web application.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Route handler for the root URL.
+    Returns Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
