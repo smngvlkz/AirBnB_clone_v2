73be774d4b8df6a691de5a702ccddfa2e1e7d761
TheCodingPantsula <asmdigital14@gmail.com>
2024-07-25 12:12:52 +0200

    C is fun!

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application with three routes.
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Returns Hello HBNB!
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Route handler for the /hbnb URL.
+    Returns "HBNB".
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_route(text):
+    """
+    Route handler for the /c/<text> URL.
+    Returns "C " followed by the value of the text variable
+    """
+    return "C {}".format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
